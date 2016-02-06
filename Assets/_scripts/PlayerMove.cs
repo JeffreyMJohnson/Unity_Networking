0966ce5534f25006d47ebd61a30f2b863b5a7ee0
JeffreyMJohnson <jefjohms@gmail.com>
2016-02-05 18:10:49 -0800

    EOD.

--- Assets/_scripts/PlayerMove.cs
+++ Assets/_scripts/PlayerMove.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
+using System;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+
+    void OnStartPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
 
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        
         float x = Input.GetAxis("Horizontal") * .1f;
         float z = Input.GetAxis("Vertical") * .1f;
-
         transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireBullet();
+        }
     }
 
+    private void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position - transform.forward, Quaternion.identity)as GameObject;
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        Destroy(bullet, 2.0f);
+    }
 }
