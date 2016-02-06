df82589915a46e5c8192bd841a0fa046a2c58e22
JeffreyMJohnson <jefjohms@gmail.com>
2016-02-05 17:38:23 -0800

    initial checkin

---
+++ Assets/_scripts/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * .1f;
+        float z = Input.GetAxis("Vertical") * .1f;
+
+        transform.Translate(x, 0, z);
+    }
+
+}
